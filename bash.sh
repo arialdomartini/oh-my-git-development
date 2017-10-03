b049325410d4d67741c50f2a0ae2a14575c12d54
Arialdo Martini <arialdomartini@gmail.com>
2017-10-03 14:27:57 +0200

    bash.sh checks the number of parameters and provides an usage help

--- bash.sh
+++ bash.sh
@@ -1,7 +1,16 @@
 set -e
 source realpath.sh
 
+usage() {
+    echo "usage: `basename $0` oh-my-git_repository_path"
+}
+
 OMGDIR=`realpath ${1}`
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    usage
+    exit -1
+fi
 
 docker run -ti \
        --name oh-my-git-bash \
